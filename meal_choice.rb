205467f653d7591847baf31817f83f8da4a1bcb2
haleykeizur <haleykz@yahoo.com>
2018-07-24 17:55:37 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -4,16 +4,16 @@ def breakfast(breakfast_food= "frosted flakes")
   puts "Morning is the best time for #{breakfast_food}!"
 end
 
-puts breakfast("pancakes".downcase))
+puts breakfast("pancakes")
 
 def lunch(lunch_food= "grilled cheese")
   puts "Afternoon is the best time for #{lunch_food}!"
 end
 
-puts lunch("peanut butter".downcase))
+puts lunch("peanut butter")
 
 def dinner(dinner_food= "salmon")
   puts "Nighttime is the best time for #{dinner_food}"
 end
 
-puts dinner("broccoli".downcase)
+puts dinner("broccoli")
